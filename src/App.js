2d5f641b76eb9a71e085a7a8f4d00141b7aa4033
Kevin <kevingcfcb88@gmail.com>
2019-09-30 21:26:20 -0600

    Nav selected stored

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import './App.css';
 
 class App extends React.Component{
 
+  state = {
+    aciveTab: 0
+  };
+
+  handleTabChange = (index) => {
+    this.setState({
+      aciveTab: index
+    });
+  }
+
   renderContent(){
-    return(
-      <span>Empty</span>
-    );
+    switch(this.state.aciveTab){
+      default: 
+      case 0: return <nav>Items</nav>;
+      case 1: return <nav>Cart</nav>;
+    }
   }
 
   render(){
+    let {activeTab} = this.state;
     return(
       <div className="App">
-        <Nav />
+        <Nav activeTab={activeTab} onTabChange={this.handleTabChange} />
         <main className="App-content">
           {this.renderContent()}
         </main>
